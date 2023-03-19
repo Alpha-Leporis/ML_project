08da84706897b0ddba79397592b1ec8278849f8e
itsmeaby <itsme.aby001@gmail.com>
2023-03-19 15:24:50 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -25,7 +25,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+
             para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
@@ -46,6 +46,14 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
